d74b594f61c56a5c73394faa41d76babf15a3bc6
Hacker_zzm <zzmhacker@gmail.com>
2019-05-09 17:16:26 +0800

    变量与常亮

--- chatper02/var_const.dart
+++ chatper02/var_const.dart
@@ -1,3 +1,22 @@
-void main(){
-  
+void main() {
+  var a;
+  print(a);
+  a = 10;
+  print(a);
+  a = "hello";
+  print(a);
+  var b = 'world';
+  print(b);
+  //final 只能赋值一次的变量，申明时必须赋值
+  final c = "hackerzzm";
+  //c = 10;
+  print(c);
+  //
+  const d = 27;
+  //d = 38;
+  print(d);
+  /**
+    https://www.jianshu.com/p/94d3fde57dfd
+    Dart中的static、final、const
+   */
 }
